882ca728a3c695af0034bc4d2f6394aad3e67ede
João <joaovictors1999@gmail.com>
2018-11-26 10:53:38 -0200

    Redirecionamento após o login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,13 @@ import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
 
     declarations: [ SigninComponent ],
-    imports: [ CommonModule, ReactiveFormsModule, VMessageModule]
+    imports: [ CommonModule, ReactiveFormsModule, VMessageModule, RouterModule]
 })
 
 export class HomeModule {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
 
@@ -12,7 +13,7 @@ export class SigninComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+    constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -26,7 +27,7 @@ export class SigninComponent implements OnInit {
         const userName = this.loginForm.get('userName').value;
         const password = this.loginForm.get('password').value;
 
-        this.authService.authenticate(userName, password).subscribe(() => console.log('autenticado'), err => {console.log(err); 
+        this.authService.authenticate(userName, password).subscribe(() => this.router.navigate(['user', userName]), err => {console.log(err); 
         this.loginForm.reset();
         alert('Invalid user name or password');
         })
