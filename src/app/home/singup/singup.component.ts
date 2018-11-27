bea06648b60d95c6f67ae757c5dce61fc971ce55
João <joaovictors1999@gmail.com>
2018-11-27 11:38:44 -0200

    Registro de novos usuário
    Tela, validação e verificação de usuários já cadastrados

--- src/app/home/singup/singup.component.ts
+++ src/app/home/singup/singup.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { validateConfig } from '@angular/router/src/config';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validators';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { NewUser } from './new-user';
+import { SingUpService } from './singup.service';
+import { Router } from '@angular/router';
+
 
 @Component({
 
@@ -11,14 +16,25 @@ export class SingUpComponent implements OnInit{
     
     singupForm: FormGroup;
     
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService, private singUpService: SingUpService, private router: Router) {}
     
     ngOnInit(): void {
         this.singupForm = this.formBuilder.group({
-            email: ['', Validators.required, Validators.email],
-            fullName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(40)],
-            username: ['', Validators.required, Validators.pattern(/^[a-z0-9_\-]+$/), Validators.minLength(2), Validators.maxLength(30)],
-            password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(14)]
-        });
-    } 
+            email:['', [Validators.required, Validators.email]],
+            fullName:['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
+            userName:['', [Validators.required, lowerCaseValidator,Validators.minLength(2), Validators.maxLength(30)], this.userNotTakenValidatorService.checkUserNameTaken()],
+            password:['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]]
+        })
+    }
+
+    singup() {
+
+        const newUser = this.singupForm.getRawValue() as NewUser;
+        this.singUpService
+        .singup(newUser)
+        .subscribe(
+            () => this.router.navigate(['']), 
+            err => console.log(err)
+        );
+    }
 }
